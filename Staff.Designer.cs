53d0991ef80660758cbc5fc5f69fea360d6e73fe
BrianDuPlessis <112475285+BrianDuPlessis@users.noreply.github.com>
2022-09-12 21:55:06 +0200

    Programmed Staff filter page

--- Staff.Designer.cs
+++ Staff.Designer.cs
@@ -41,8 +41,12 @@ namespace B4_Plastics_SMS
             this.label1 = new System.Windows.Forms.Label();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabSearch = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmbUserType = new System.Windows.Forms.ComboBox();
+            this.label20 = new System.Windows.Forms.Label();
             this.tabInsert = new System.Windows.Forms.TabPage();
             this.gbxInsertAdd = new System.Windows.Forms.GroupBox();
+            this.btnAdd = new System.Windows.Forms.Button();
             this.txtIREPassword = new System.Windows.Forms.TextBox();
             this.label11 = new System.Windows.Forms.Label();
             this.txtIPassword = new System.Windows.Forms.TextBox();
@@ -55,9 +59,9 @@ namespace B4_Plastics_SMS
             this.txtIFirstName = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.btnAdd = new System.Windows.Forms.Button();
             this.tabUpdate = new System.Windows.Forms.TabPage();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbUpdateStaff = new System.Windows.Forms.ComboBox();
             this.btnUpdate = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
@@ -66,34 +70,30 @@ namespace B4_Plastics_SMS
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label13 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
             this.label14 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.label15 = new System.Windows.Forms.Label();
             this.label16 = new System.Windows.Forms.Label();
-            this.cbUpdateStaff = new System.Windows.Forms.ComboBox();
-            this.label17 = new System.Windows.Forms.Label();
             this.tabDelete = new System.Windows.Forms.TabPage();
             this.gbxDelete = new System.Windows.Forms.GroupBox();
             this.cbConfirm = new System.Windows.Forms.CheckBox();
             this.cbDeleteStaff = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
             this.btnDelete = new System.Windows.Forms.Button();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label20 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.gbxEmployeeDetails.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvEmployeeDetails)).BeginInit();
             this.gbxSearch.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabSearch.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.tabInsert.SuspendLayout();
             this.gbxInsertAdd.SuspendLayout();
             this.tabUpdate.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.tabDelete.SuspendLayout();
             this.gbxDelete.SuspendLayout();
-            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // gbxEmployeeDetails
@@ -144,6 +144,7 @@ namespace B4_Plastics_SMS
             this.txtFilterLastName.Name = "txtFilterLastName";
             this.txtFilterLastName.Size = new System.Drawing.Size(217, 27);
             this.txtFilterLastName.TabIndex = 2;
+            this.txtFilterLastName.TextChanged += new System.EventHandler(this.txtFilterLastName_TextChanged);
             // 
             // txtFilterName
             // 
@@ -152,6 +153,7 @@ namespace B4_Plastics_SMS
             this.txtFilterName.Name = "txtFilterName";
             this.txtFilterName.Size = new System.Drawing.Size(217, 27);
             this.txtFilterName.TabIndex = 1;
+            this.txtFilterName.TextChanged += new System.EventHandler(this.txtFilterName_TextChanged);
             // 
             // label3
             // 
@@ -171,6 +173,7 @@ namespace B4_Plastics_SMS
             this.txtFilterEmail.Name = "txtFilterEmail";
             this.txtFilterEmail.Size = new System.Drawing.Size(217, 27);
             this.txtFilterEmail.TabIndex = 0;
+            this.txtFilterEmail.TextChanged += new System.EventHandler(this.txtFilterEmail_TextChanged);
             // 
             // label2
             // 
@@ -221,6 +224,41 @@ namespace B4_Plastics_SMS
             this.tabSearch.Text = "Search";
             this.tabSearch.UseVisualStyleBackColor = true;
             // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.White;
+            this.groupBox2.Controls.Add(this.cmbUserType);
+            this.groupBox2.Controls.Add(this.label20);
+            this.groupBox2.Font = new System.Drawing.Font("Nirmala UI Semilight", 13F);
+            this.groupBox2.ForeColor = System.Drawing.Color.Navy;
+            this.groupBox2.Location = new System.Drawing.Point(524, 490);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(414, 277);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Filter Staff by:";
+            // 
+            // cmbUserType
+            // 
+            this.cmbUserType.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.cmbUserType.FormattingEnabled = true;
+            this.cmbUserType.Location = new System.Drawing.Point(150, 75);
+            this.cmbUserType.Name = "cmbUserType";
+            this.cmbUserType.Size = new System.Drawing.Size(217, 28);
+            this.cmbUserType.TabIndex = 2;
+            this.cmbUserType.SelectedIndexChanged += new System.EventHandler(this.cmbUserType_SelectedIndexChanged);
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Font = new System.Drawing.Font("Nirmala UI", 11F);
+            this.label20.ForeColor = System.Drawing.Color.Black;
+            this.label20.Location = new System.Drawing.Point(21, 72);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(98, 25);
+            this.label20.TabIndex = 1;
+            this.label20.Text = "User Type:";
+            // 
             // tabInsert
             // 
             this.tabInsert.Controls.Add(this.gbxInsertAdd);
@@ -256,6 +294,20 @@ namespace B4_Plastics_SMS
             this.gbxInsertAdd.TabIndex = 6;
             this.gbxInsertAdd.TabStop = false;
             this.gbxInsertAdd.Text = "Insert/ Add Staff";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(79)))), ((int)(((byte)(211)))));
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(234, 415);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(265, 55);
+            this.btnAdd.TabIndex = 21;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
             // 
             // txtIREPassword
             // 
@@ -371,20 +423,6 @@ namespace B4_Plastics_SMS
             this.label6.TabIndex = 8;
             this.label6.Text = "First name:";
             // 
-            // btnAdd
-            // 
-            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(79)))), ((int)(((byte)(211)))));
-            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.ForeColor = System.Drawing.Color.White;
-            this.btnAdd.Location = new System.Drawing.Point(234, 415);
-            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(265, 55);
-            this.btnAdd.TabIndex = 21;
-            this.btnAdd.Text = "&Add";
-            this.btnAdd.UseVisualStyleBackColor = false;
-            // 
             // tabUpdate
             // 
             this.tabUpdate.Controls.Add(this.groupBox1);
@@ -422,6 +460,15 @@ namespace B4_Plastics_SMS
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Update Staff Details";
+            // 
+            // cbUpdateStaff
+            // 
+            this.cbUpdateStaff.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.cbUpdateStaff.FormattingEnabled = true;
+            this.cbUpdateStaff.Location = new System.Drawing.Point(234, 58);
+            this.cbUpdateStaff.Name = "cbUpdateStaff";
+            this.cbUpdateStaff.Size = new System.Drawing.Size(265, 28);
+            this.cbUpdateStaff.TabIndex = 22;
             // 
             // btnUpdate
             // 
@@ -502,6 +549,17 @@ namespace B4_Plastics_SMS
             this.label13.TabIndex = 14;
             this.label13.Text = "Contact number:";
             // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Nirmala UI", 11F);
+            this.label17.ForeColor = System.Drawing.Color.Black;
+            this.label17.Location = new System.Drawing.Point(120, 61);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(76, 25);
+            this.label17.TabIndex = 13;
+            this.label17.Text = "Staff ID:";
+            // 
             // label14
             // 
             this.label14.AutoSize = true;
@@ -550,26 +608,6 @@ namespace B4_Plastics_SMS
             this.label16.Size = new System.Drawing.Size(103, 25);
             this.label16.TabIndex = 8;
             this.label16.Text = "First name:";
-            // 
-            // cbUpdateStaff
-            // 
-            this.cbUpdateStaff.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.cbUpdateStaff.FormattingEnabled = true;
-            this.cbUpdateStaff.Location = new System.Drawing.Point(234, 58);
-            this.cbUpdateStaff.Name = "cbUpdateStaff";
-            this.cbUpdateStaff.Size = new System.Drawing.Size(265, 28);
-            this.cbUpdateStaff.TabIndex = 22;
-            // 
-            // label17
-            // 
-            this.label17.AutoSize = true;
-            this.label17.Font = new System.Drawing.Font("Nirmala UI", 11F);
-            this.label17.ForeColor = System.Drawing.Color.Black;
-            this.label17.Location = new System.Drawing.Point(120, 61);
-            this.label17.Name = "label17";
-            this.label17.Size = new System.Drawing.Size(76, 25);
-            this.label17.TabIndex = 13;
-            this.label17.Text = "Staff ID:";
             // 
             // tabDelete
             // 
@@ -644,40 +682,6 @@ namespace B4_Plastics_SMS
             this.btnDelete.Text = "&Delete";
             this.btnDelete.UseVisualStyleBackColor = false;
             // 
-            // groupBox2
-            // 
-            this.groupBox2.BackColor = System.Drawing.Color.White;
-            this.groupBox2.Controls.Add(this.comboBox1);
-            this.groupBox2.Controls.Add(this.label20);
-            this.groupBox2.Font = new System.Drawing.Font("Nirmala UI Semilight", 13F);
-            this.groupBox2.ForeColor = System.Drawing.Color.Navy;
-            this.groupBox2.Location = new System.Drawing.Point(524, 490);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(414, 277);
-            this.groupBox2.TabIndex = 4;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Filter Staff by:";
-            // 
-            // label20
-            // 
-            this.label20.AutoSize = true;
-            this.label20.Font = new System.Drawing.Font("Nirmala UI", 11F);
-            this.label20.ForeColor = System.Drawing.Color.Black;
-            this.label20.Location = new System.Drawing.Point(21, 72);
-            this.label20.Name = "label20";
-            this.label20.Size = new System.Drawing.Size(123, 31);
-            this.label20.TabIndex = 1;
-            this.label20.Text = "User Type:";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(150, 75);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(217, 28);
-            this.comboBox1.TabIndex = 2;
-            // 
             // Staff
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -687,12 +691,15 @@ namespace B4_Plastics_SMS
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Staff";
             this.Text = "Staff";
+            this.Activated += new System.EventHandler(this.Staff_Activated);
             this.gbxEmployeeDetails.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvEmployeeDetails)).EndInit();
             this.gbxSearch.ResumeLayout(false);
             this.gbxSearch.PerformLayout();
             this.tabControl1.ResumeLayout(false);
             this.tabSearch.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.tabInsert.ResumeLayout(false);
             this.gbxInsertAdd.ResumeLayout(false);
             this.gbxInsertAdd.PerformLayout();
@@ -702,8 +709,6 @@ namespace B4_Plastics_SMS
             this.tabDelete.ResumeLayout(false);
             this.gbxDelete.ResumeLayout(false);
             this.gbxDelete.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -760,7 +765,7 @@ namespace B4_Plastics_SMS
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cmbUserType;
         private System.Windows.Forms.Label label20;
     }
 }
--- Staff.cs
+++ Staff.cs
@@ -7,14 +7,110 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace B4_Plastics_SMS
 {
     public partial class Staff : Form
     {
+        public static string conStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\CMPG223-Project\B4_Plastics_SMS\B4Plastics.mdf;Integrated Security=True";
+        SqlConnection conn = new SqlConnection(conStr);
+        SqlDataAdapter adapter;
+        DataSet dataset;
+        SqlCommand command;
+        SqlDataReader reader;
+
         public Staff()
         {
             InitializeComponent();
+        }
+
+        private void displayData(string sql)
+        {
+            try
+            {
+                conn.Open();
+                command = new SqlCommand(sql, conn);
+                adapter.SelectCommand = command;
+                adapter.Fill(dataset, "Staff");
+
+                dgvEmployeeDetails.DataSource = dataset;
+                dgvEmployeeDetails.DataMember = "Staff";
+                conn.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Error - Sorry, something went wrong");
+            }
+        }
+
+        private void Staff_Activated(object sender, EventArgs e)
+        {
+            //display data
+            string DisplayAllsql = "SELECT * FROM Staff";
+            displayData(DisplayAllsql);
+
+            //fill comboBox UserType
+            string fillBoxSql = "SELECT staff_usertype FROM Staff";
+            try
+            {
+                conn.Open();
+                command = new SqlCommand(fillBoxSql, conn);
+                
+                conn.Close();
+            }
+            catch(SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+        }
+
+        private void txtFilterEmail_TextChanged(object sender, EventArgs e)
+        {
+            txtFilterName.Text = "";
+            txtFilterLastName.Text = "";
+
+            string sql = $"SELECT * FROM Staff WHERE staff_email LIKE '{txtFilterEmail.Text}%'";
+            displayData(sql);
+        }
+
+        private void txtFilterName_TextChanged(object sender, EventArgs e)
+        {
+            txtFilterEmail.Text = "";
+            txtFilterLastName.Text = "";
+
+            string sql = $"SELECT * FROM Staff WHERE staff_name LIKE '{txtFilterName.Text}%'";
+            displayData(sql);
+        }
+
+        private void txtFilterLastName_TextChanged(object sender, EventArgs e)
+        {
+            txtFilterEmail.Text = "";
+            txtFilterName.Text = "";
+
+            string sql = $"SELECT * FROM Staff WHERE staff_lastname LIKE '{txtFilterLastName.Text}%'";
+            displayData(sql);
+        }
+
+        private void cmbUserType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string extraFilter = "", sql = "";
+            if (txtFilterEmail.Text != "")
+            {
+                extraFilter = "AND WHERE staff_email LIKE '"+ txtFilterEmail.Text + "%'";
+            }
+            else if (txtFilterName.Text != "")
+            {
+                extraFilter = "AND WHERE staff_name LIKE '" + txtFilterName.Text + "%'";
+            }
+            else if (txtFilterLastName.Text != "")
+            {
+                extraFilter = "AND WHERE staff_lastname LIKE '" + txtFilterLastName.Text + "%'";
+            }
+
+            sql = $"SELECT * FROM Staff WHERE staff_usertype = '{cmbUserType.SelectedItem.ToString()}'" + extraFilter;
+            displayData(sql);
         }
     }
 }
