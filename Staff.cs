848efee6b70b090d22af1ad684d9789aad793fa3
BrianDuPlessis <112475285+BrianDuPlessis@users.noreply.github.com>
2022-09-14 12:02:36 +0200

    Finished stock delete and insert pages

--- Staff.cs
+++ Staff.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.IO;
 
 namespace B4_Plastics_SMS
 {
@@ -31,6 +32,8 @@ namespace B4_Plastics_SMS
             {
                 conn.Open();
                 command = new SqlCommand(sql, conn);
+                adapter = new SqlDataAdapter();
+                dataset = new DataSet();
                 adapter.SelectCommand = command;
                 adapter.Fill(dataset, "Staff");
 
@@ -38,9 +41,9 @@ namespace B4_Plastics_SMS
                 dgvEmployeeDetails.DataMember = "Staff";
                 conn.Close();
             }
-            catch
+            catch(SqlException error)
             {
-                MessageBox.Show("Error - Sorry, something went wrong");
+                MessageBox.Show(error.Message);
             }
         }
 
@@ -51,12 +54,18 @@ namespace B4_Plastics_SMS
             displayData(DisplayAllsql);
 
             //fill comboBox UserType
-            string fillBoxSql = "SELECT staff_usertype FROM Staff";
+            string fillBoxSql = "SELECT DISTINCT staff_usertype FROM Staff";
             try
             {
                 conn.Open();
                 command = new SqlCommand(fillBoxSql, conn);
-                
+                reader = command.ExecuteReader();
+                cmbUserType.Items.Clear();
+                while (reader.Read())
+                {
+                    cmbUserType.Items.Add(reader.GetValue(0));
+                    cmbIUserType.Items.Add(reader.GetValue(0));
+                }
                 conn.Close();
             }
             catch(SqlException error)
@@ -64,6 +73,46 @@ namespace B4_Plastics_SMS
                 MessageBox.Show(error.Message);
             }
 
+            //fill delete comboBox
+            string StaffIDSql = "SELECT staff_id FROM Staff";
+            try
+            {
+                conn.Open();
+                command = new SqlCommand(StaffIDSql, conn);
+                reader = command.ExecuteReader();
+                cmbDeleteStaff.Items.Clear();
+                while (reader.Read())
+                {
+                    cmbDeleteStaff.Items.Add(reader.GetValue(0));
+                }
+                conn.Close();
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+            txtIPassword.PasswordChar = '*';
+            txtIREPassword.PasswordChar = '*';
+
+            //cbUpdateSatff, fill comboBox
+            string updateIdSql = "SELECT staff_id FROM Staff";
+            try
+            {
+                conn.Open();
+                command = new SqlCommand(updateIdSql, conn);
+                reader = command.ExecuteReader();
+                cbUpdateStaff.Items.Clear();
+                while (reader.Read())
+                {
+                    cbUpdateStaff.Items.Add(reader.GetValue(0));
+                }
+                conn.Close();
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
         }
 
         private void txtFilterEmail_TextChanged(object sender, EventArgs e)
@@ -71,7 +120,7 @@ namespace B4_Plastics_SMS
             txtFilterName.Text = "";
             txtFilterLastName.Text = "";
 
-            string sql = $"SELECT * FROM Staff WHERE staff_email LIKE '{txtFilterEmail.Text}%'";
+            string sql = generateFilterSql();
             displayData(sql);
         }
 
@@ -80,7 +129,7 @@ namespace B4_Plastics_SMS
             txtFilterEmail.Text = "";
             txtFilterLastName.Text = "";
 
-            string sql = $"SELECT * FROM Staff WHERE staff_name LIKE '{txtFilterName.Text}%'";
+            string sql = generateFilterSql();
             displayData(sql);
         }
 
@@ -89,28 +138,192 @@ namespace B4_Plastics_SMS
             txtFilterEmail.Text = "";
             txtFilterName.Text = "";
 
-            string sql = $"SELECT * FROM Staff WHERE staff_lastname LIKE '{txtFilterLastName.Text}%'";
+            string sql = generateFilterSql();
             displayData(sql);
         }
 
-        private void cmbUserType_SelectedIndexChanged(object sender, EventArgs e)
+        private string generateFilterSql()
         {
             string extraFilter = "", sql = "";
             if (txtFilterEmail.Text != "")
             {
-                extraFilter = "AND WHERE staff_email LIKE '"+ txtFilterEmail.Text + "%'";
+                sql = $"SELECT * FROM Staff WHERE staff_email LIKE '{txtFilterEmail.Text}%'";
             }
             else if (txtFilterName.Text != "")
             {
-                extraFilter = "AND WHERE staff_name LIKE '" + txtFilterName.Text + "%'";
+                sql = $"SELECT * FROM Staff WHERE staff_name LIKE '{txtFilterName.Text}%'";
             }
             else if (txtFilterLastName.Text != "")
             {
-                extraFilter = "AND WHERE staff_lastname LIKE '" + txtFilterLastName.Text + "%'";
+                sql = $"SELECT * FROM Staff WHERE staff_lastname LIKE '{txtFilterLastName.Text}%'";
+            }
+            else
+            {
+                sql = "SELECT * FROM Staff ";
             }
 
-            sql = $"SELECT * FROM Staff WHERE staff_usertype = '{cmbUserType.SelectedItem.ToString()}'" + extraFilter;
+            if (cmbUserType.SelectedIndex > -1 && sql.Contains("WHERE"))
+            {
+                extraFilter = $" AND staff_usertype like '{cmbUserType.SelectedItem.ToString()}'";
+            }
+            else if (cmbUserType.SelectedIndex > -1)
+            {
+                extraFilter = $" WHERE staff_usertype like '{cmbUserType.SelectedItem.ToString()}'";
+            }
+           
+            return sql + extraFilter;
+        }
+
+        private void cmbUserType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string sql = generateFilterSql();
             displayData(sql);
+        }
+
+        private void btnDisplayAll_Click(object sender, EventArgs e)
+        {
+            //display data
+            string DisplayAllsql = "SELECT * FROM Staff";
+            displayData(DisplayAllsql);
+
+            //Clear filters
+            txtFilterEmail.Text = "";
+            txtFilterLastName.Text = "";
+            txtFilterName.Text = "";
+            cmbUserType.SelectedIndex = -1;
+        }
+
+        private bool validateStaffRecord()
+        {
+            bool valid = true;
+
+            txtIEmail.BackColor = SystemColors.Window;
+            txtIFirstName.BackColor = SystemColors.Window;
+            txtILastName.BackColor = SystemColors.Window;
+            txtIContactNumber.BackColor = SystemColors.Window;
+            txtIPassword.BackColor = SystemColors.Window;
+            txtIREPassword.BackColor = SystemColors.Window;
+            cmbIUserType.BackColor = SystemColors.Window;
+            if (txtIEmail.Text == "" || !txtIEmail.Text.Contains("@"))
+            {
+                txtIEmail.Focus();
+                txtIEmail.BackColor = Color.Red;
+                valid = false;
+            }
+            if (txtIFirstName.Text == "" || txtIFirstName.Text.Length < 3)
+            {
+                txtIFirstName.Focus();
+                txtIFirstName.BackColor = Color.Red;
+                valid = false;
+            }
+            if (txtILastName.Text == "" || txtILastName.Text.Length < 3)
+            {
+                txtILastName.Focus();
+                txtILastName.BackColor = Color.Red;
+                valid = false;
+            }
+            if (!int.TryParse(txtIContactNumber.Text, out int num) && txtIContactNumber.Text.Length < 10)
+            {
+                txtIContactNumber.Focus();
+                txtIContactNumber.BackColor = Color.Red;
+                valid = false;
+            }
+            if (cmbIUserType.SelectedIndex == -1)
+            {
+                cmbIUserType.Focus();
+                cmbIUserType.BackColor = Color.Red;
+                valid = false;
+            }
+            if (txtIPassword.Text.Length < 5)
+            {
+                txtIPassword.Focus();
+                txtIPassword.BackColor = Color.Red;
+                valid = false;
+            }
+            if (txtIREPassword.Text != txtIPassword.Text || txtIREPassword.Text == "")
+            {
+                txtIREPassword.Focus();
+                txtIREPassword.BackColor = Color.Red;
+                valid = false;
+            }
+            return valid;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (validateStaffRecord())
+            {
+                try
+                {
+                    //Save user password
+                    StreamWriter writer = new StreamWriter("Account.txt");
+                    writer.WriteLine(txtIEmail + " " + txtIPassword);
+                    writer.Close();
+
+                    //Add record to database
+                    conn.Open();
+                    string sql = "INSERT INTO Staff(staff_email, staff_name, staff_lastname, staff_cell, staff_usertype) VALUES(@email, @name, @lastname, @cell, @type)";
+                    command = new SqlCommand(sql, conn); 
+                    command.Parameters.AddWithValue("@email", txtIEmail.Text);
+                    command.Parameters.AddWithValue("@name", txtIFirstName.Text);
+                    command.Parameters.AddWithValue("@lastname", txtILastName.Text);
+                    command.Parameters.AddWithValue("@cell", txtIContactNumber.Text);
+                    command.Parameters.AddWithValue("@type", cmbIUserType.SelectedItem.ToString());
+                    command.ExecuteNonQuery();
+                    conn.Close();
+
+                    //clear input
+                    txtIEmail.Clear();
+                    txtIFirstName.Clear();
+                    txtILastName.Clear();
+                    txtIContactNumber.Clear();
+                    txtIPassword.Clear();
+                    txtIREPassword.Clear();
+                    cmbIUserType.SelectedItem = -1;
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+                finally
+                {
+                    MessageBox.Show("New staff member was successfully added");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid data...");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (cbConfirm.Checked)
+            {
+                string sql = $"DELETE FROM Staff WHERE staff_id = {cmbDeleteStaff.SelectedIndex.ToString()}";
+                try
+                {
+                    conn.Open();
+                    command = new SqlCommand(sql, conn);
+                    command.ExecuteNonQuery();
+                    conn.Close();
+
+                    //Notify user
+                    MessageBox.Show("Staff member was successfully removed");
+
+                    cmbDeleteStaff.SelectedIndex = -1;
+                    cmbDeleteStaff.Text = "";
+                    cbConfirm.Checked = false;
+                }
+                catch(SqlException error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wait! Please confirm delete operation");
+            }
         }
     }
 }
